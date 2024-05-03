ac9c9d1cea7f420c6c8e8e4f79c238d6414b7e25
vuzhvlad <vuzh.vlad@gmail.com>
2024-05-03 10:45:17 +0200

    working with get request for tours

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+const fs = require('fs');
 const express = require('express'); // adding methods from express
 
 const app = express();
 
 // routing, if someone hits the / it will do something in the callback
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' }); // sending status and json back
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' }); // sending status and json back
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+//getting the json file with all the tours: parsing the file with json we ve read
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
