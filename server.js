50e6e3e40a9f56f54209ed1d2ec92a9738741577
vuzhvlad <vuzh.vlad@gmail.com>
2024-06-03 11:51:56 +0200

    connecting db, creating schema and model by using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); // read our variables from the file and save them in the environment
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }) //connecting database via mongoose to our backend
+  .then(() => console.log('db connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  // creating schema
+  name: {
+    // schema type object
+    type: String,
+    required: [true, 'A tour must have a name'], // required and an error for it
+    unique: true, // we cant have 2 tour documents with the same name
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema); // creating model out of the schema, (model name, schema)
 
 const port = process.env.PORT;
 //creating server
