d15233117da6037e98f5c0c0f9a55e5f3612c04c
vuzhvlad <vuzh.vlad@gmail.com>
2024-06-17 13:57:16 +0200

    deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -85,10 +85,19 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    // sending 204 and null for deleting something
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      // sending 204 and null for deleting something
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
